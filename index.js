b77d93373aab7d3210dc220d0aafb6ee2ba2bb58
newswim <danminshew@gmail.com>
2017-11-08 10:40:56 -0600

    Add webpack dependencies and basic config file

---
+++ index.js
@@ -0,0 +1,1 @@
+'antd/dist/antd.less'
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,4 @@
-// TODO: point this to node_modules/antd/components
+// TODO: point this to .node_modules/antd/lib
 module.exports = {
   components: 'src/components/**/[A-Z]*.js'
 };
