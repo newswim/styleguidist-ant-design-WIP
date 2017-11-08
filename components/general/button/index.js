cf5abaeacce62010753b6d7e88ac74d05b7cd52d
newswim <danminshew@gmail.com>
2017-11-08 11:28:58 -0600

    Add @babel/preset-env rather than babel-preset-env

---
+++ components/general/button/index.js
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+
+const ButtonExample = () => [
+  <Button type="primary">Primary</Button>,
+  <Button>Default</Button>,
+  <Button type="dashed">Dashed</Button>,
+  <Button type="danger">Danger</Button>
+]
+
+export default ButtonExample
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ module.exports = () => {
     module: {
       rules: [
         {
+          // JavaScript
           test: /\.js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['import', { libraryName: 'antd', style: true }]
             }
           }
         }
